40be2993055079c9e374433822c9d5078088ab33
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-01 19:08:22 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,9 +1,10 @@
 require 'pry'
 
 def my_find(collection)
-i = 0
+ i = 0
   while i < collection.length
-    yield(collection[i])
+    return collection[i] if
+  yield(collection[i])
     i = i + 1
   end
 end
